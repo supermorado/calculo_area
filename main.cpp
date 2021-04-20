f7cb7e9d72f5bd9ff1ba1ffc2a265b8847c9a633
Profe Ale <>
2021-04-20 00:37:58 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//realizado por Alejandro Monge
+//Cálculo del Área de un triángulo
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+
+int base;
+int altura;
+int area;
+
+cout<<"Digite el valor de la base: ";
+cin>>base;
+cout<<"Digite el valor de la altura: ";
+cin>>altura;
+area=(base*altura)/2;
+cout<<"El area es = "<<area<<endl;
+
+}
